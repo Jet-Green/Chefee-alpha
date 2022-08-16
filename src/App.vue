22dc0bbfe216c06e26a37e1916c18ff587dcdd13
Grisha Dzyin <grishadzyin@gmail.com>
2022-08-16 18:57:13 +0400

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,37 @@
 <template>
   <v-layout>
     <v-app-bar app>
-      <v-row>
-        <v-col>
+      <v-row class="d-flex justify-space-between align-center">
+        <v-col class="d-flex align-center" lg="2">
           logo
         </v-col>
-        <v-col>
+
+        <v-col class="d-flex align-center" lg="5">
           <v-text-field variant="outlined" density="compact" placeholder="Поиск по ингридиентам или по названию"
             hide-details="auto"></v-text-field>
         </v-col>
-        <v-col>
-          <v-btn color="primary">Поиск по фото</v-btn>
+
+        <v-col class="d-flex align-center" lg="3">
+          <v-btn color="accent" class="header-accent-button">Поиск по фото</v-btn>
         </v-col>
-        <v-col>
-          войти
+
+        <v-col class="d-md-flex d-none align-center justify-end" cols="2">
+          <v-btn>
+            войти
+          </v-btn>
+          <v-btn>
+            регистрация
+          </v-btn>
         </v-col>
-        <v-col>
-          регистрация
+        <v-col class="d-md-none d-block" cols="1">
+          <v-btn icon="mdi:mdi-menu"></v-btn>
         </v-col>
+
       </v-row>
     </v-app-bar>
     <v-main>
       <v-container>
-        <v-row class="d-flex justify-center">
-          <v-col cols="12" md="10" lg="6">
-            <router-view />
-          </v-col>
-        </v-row>
+        <router-view />
       </v-container>
     </v-main>
   </v-layout>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -5,6 +5,21 @@ import 'vuetify/styles'
 // Vuetify
 import { createVuetify } from 'vuetify'
 
-export default createVuetify(
-  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
-)
+export default createVuetify({
+  theme: {
+    themes: {
+      light: {
+        colors: {
+          primary: '#1976D2',
+          // secondary: '#424242',
+          accent: '#FE9800',
+          // error: '#FF5252',
+          // info: '#2196F3',
+          // success: '#4CAF50',
+          // warning: '#FFC107',
+        }
+      },
+    },
+  },
+})
+// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
