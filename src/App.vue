03d25e69e2d1f2bfedcce653b76337153ea019d5
Jet-Green <grishadzyin@gmail.com>
2022-10-24 19:42:38 +0400

    login and reg to nav drawer

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,8 @@ let loginPassword = ref('')
 let registrationEmail = ref('')
 let registrationPassword = ref('')
 
+let drawer = ref(false)
+
 function search() {
     if (searchRequest.value.trim()) {
         useRecipesStore.setSearchRequest(searchRequest.value.trim());
@@ -29,6 +31,7 @@ function search() {
 let loginDialog = ref(false)
 function openLoginDialog() {
     loginDialog.value = true;
+    registrationDialog.value = false;
 }
 function login() {
     userStore.login(loginEmail.value, loginPassword.value)
@@ -37,6 +40,7 @@ function login() {
 let registrationDialog = ref(false)
 function openRegistrationDialog() {
     registrationDialog.value = true;
+    loginDialog.value = false;
 }
 function registration() {
     userStore.registration(registrationEmail.value, registrationPassword.value)
@@ -50,11 +54,28 @@ onMounted(() => {
 })
 </script>
 <template>
-    <v-layout>
+    <v-app>
+        <v-navigation-drawer app v-model="drawer" right temporary>
+            <v-list>
+                <v-list-item v-if="userStore.isAuth">
+                    <v-btn @click="userStore.logout">
+                        выйти
+                    </v-btn>
+                </v-list-item>
+                <v-list-item v-if="!userStore.isAuth">
+                    <v-list-item-title @click="openLoginDialog">войти</v-list-item-title>
+                </v-list-item>
+
+                <v-list-item v-if="!userStore.isAuth">
+                    <v-list-item-title @click="openRegistrationDialog">регистрация</v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+
         <v-app-bar elevation="0" app>
             <v-row class="d-flex justify-space-between align-center">
                 <v-col class="d-flex align-center jusitfy-center" cols="2">
-                    <img :src="logo" class="ml-1" />
+                    <img :src="logo" class="ml-5" />
                 </v-col>
 
                 <v-col class="d-none d-md-flex align-center" cols="8">
@@ -69,100 +90,82 @@ onMounted(() => {
                     </v-form>
                     <v-btn class="accent-button ml-4">Поиск по фото</v-btn>
                 </v-col>
-
-                <v-col class="d-lg-flex d-none align-center justify-end" cols="2">
-                    <v-btn v-if="userStore.isAuth" @click="userStore.logout">
-                        выйти
-                    </v-btn>
-
-                    <div v-else>
-                        <v-btn @click="openLoginDialog">
-                            войти
-                        </v-btn>
-                        <v-dialog v-model="loginDialog">
-                            <v-card style="min-width: 40vw">
-                                <v-card-title>
-                                    <h3>Вход</h3>
-                                </v-card-title>
-                                <v-card-text>
-                                    <v-container>
-                                        <v-row>
-                                            <v-col cols="12">
-                                                <v-text-field v-model="loginEmail" placeholder="email"
-                                                    variant="outlined" hide-details="auto" density="compact"
-                                                    class="search-input">
-                                                </v-text-field>
-                                            </v-col>
-                                            <v-col cols="12">
-                                                <v-text-field v-model="loginPassword" placeholder="пароль"
-                                                    variant="outlined" hide-details="auto" density="compact"
-                                                    class="search-input" type="password">
-                                                </v-text-field>
-                                            </v-col>
-                                        </v-row>
-                                    </v-container>
-                                </v-card-text>
-                                <v-card-actions>
-                                    <v-btn @click="login">
-                                        войти
-                                    </v-btn>
-                                    <v-btn @click="openRegistrationDialog">
-                                        регистрация
-                                    </v-btn>
-                                </v-card-actions>
-                            </v-card>
-                        </v-dialog>
-
-
-                        <v-btn @click="openRegistrationDialog">
-                            регистрация
-                        </v-btn>
-                        <v-dialog v-model="registrationDialog">
-                            <v-card style="min-width: 30vw">
-                                <v-card-title>
-                                    <h3>Регистрация</h3>
-                                </v-card-title>
-                                <v-card-text>
-                                    <v-container>
-                                        <v-row>
-                                            <v-col cols="12">
-                                                <v-text-field v-model="registrationEmail" placeholder="email"
-                                                    variant="outlined" hide-details="auto" density="compact"
-                                                    class="search-input">
-                                                </v-text-field>
-                                            </v-col>
-                                            <v-col cols="12">
-                                                <v-text-field v-model="registrationPassword" placeholder="пароль"
-                                                    variant="outlined" hide-details="auto" density="compact"
-                                                    class="search-input" type="password">
-                                                </v-text-field>
-                                            </v-col>
-                                        </v-row>
-                                    </v-container>
-                                </v-card-text>
-                                <v-card-actions>
-                                    <v-btn @click="registration">
-                                        зарегистрироваться
-                                    </v-btn>
-                                    <v-btn @click="openLoginDialog">
-                                        войти
-                                    </v-btn>
-                                </v-card-actions>
-                            </v-card>
-                        </v-dialog>
-                    </div>
-                </v-col>
-                <v-col class="d-lg-none d-flex justify-end" cols="2">
-                    <v-btn @click="openLoginDialog" class="d-flex align-center">
+                <v-spacer></v-spacer>
+                <v-col class="d-flex justify-end">
+                    <v-btn @click="drawer = !drawer" class="d-flex align-center">
                         <span class="material-icons ma-3">menu</span>
                     </v-btn>
                 </v-col>
             </v-row>
         </v-app-bar>
+
         <v-main>
             <v-container>
                 <router-view />
             </v-container>
         </v-main>
-    </v-layout>
+
+        <v-dialog v-model="loginDialog">
+            <v-card style="min-width: 30vw">
+                <v-card-title>
+                    <h3>Вход</h3>
+                </v-card-title>
+                <v-card-text>
+                    <v-container>
+                        <v-row>
+                            <v-col cols="12">
+                                <v-text-field v-model="loginEmail" placeholder="email" variant="outlined"
+                                    hide-details="auto" density="compact" class="search-input">
+                                </v-text-field>
+                            </v-col>
+                            <v-col cols="12">
+                                <v-text-field v-model="loginPassword" placeholder="пароль" variant="outlined"
+                                    hide-details="auto" density="compact" class="search-input" type="password">
+                                </v-text-field>
+                            </v-col>
+                        </v-row>
+                    </v-container>
+                </v-card-text>
+                <v-card-actions>
+                    <v-btn @click="login">
+                        войти
+                    </v-btn>
+                    <v-btn @click="openRegistrationDialog">
+                        регистрация
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+        <v-dialog v-model="registrationDialog">
+            <v-card style="min-width: 30vw" class="d-flex">
+                <v-card-title>
+                    <h3>Регистрация</h3>
+                </v-card-title>
+                <v-card-text>
+                    <v-container>
+                        <v-row>
+                            <v-col cols="12">
+                                <v-text-field v-model="registrationEmail" placeholder="email" variant="outlined"
+                                    hide-details="auto" density="compact" class="search-input">
+                                </v-text-field>
+                            </v-col>
+                            <v-col cols="12">
+                                <v-text-field v-model="registrationPassword" placeholder="пароль" variant="outlined"
+                                    hide-details="auto" density="compact" class="search-input" type="password">
+                                </v-text-field>
+                            </v-col>
+                        </v-row>
+                    </v-container>
+                </v-card-text>
+                <v-card-actions>
+                    <v-btn @click="registration">
+                        зарегистрироваться
+                    </v-btn>
+                    <v-btn @click="openLoginDialog">
+                        вход
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+    </v-app>
 </template>
