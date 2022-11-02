c6ae9a66fadd06899feb80660ae29bcdd8283f90
Jet-Green <grishadzyin@gmail.com>
2022-11-02 16:56:13 +0400

    fix icons and continue addrecipe

--- src/components/dev/AddRecipe.vue
+++ src/components/dev/AddRecipe.vue
@@ -1,5 +1,29 @@
 <script setup>
+import { reactive } from 'vue';
+let recipe = reactive({
+  author: 'Chefee',
+  title: "",
+  previewImage: '',
+  ingredients: [],
+  portions: 0,
+  time: "",
+  likes: 0,
+  likedBy: [],
+  reposts: 0,
+  comments: [],
+  steps: [],
+  description: "",
+  health: {
+    protein: 0,
+    fat: 0,
+    carbohydrates: 0,
+    kcal: 0
+  }
+})
 
+const ingredientNameVariants = [
+  "Репчатый Лук", "Соевый соус", "Сыр"
+]
 </script>
 <template>
   <v-row class="d-flex jusitfy-center">
@@ -19,6 +43,23 @@
         </v-col>
         <v-col cols="6">
           <v-text-field clearable label="description" density="compact" variant="outlined"></v-text-field>
+        </v-col>
+        <v-col cols="12">
+          <h3>Ingredients</h3>
+          <v-list>
+            <v-list-item v-for="ingr of recipe.ingredients"></v-list-item>
+          </v-list>
+        </v-col>
+        <v-col cols="6">
+          <v-autocomplete label="name" variant="outlined" density="compact" :items="ingredientNameVariants">
+          </v-autocomplete>
+        </v-col>
+        <v-col cols="3">
+          <v-text-field clearable type="number" label="amount number" density="compact" variant="outlined">
+          </v-text-field>
+        </v-col>
+        <v-col cols="3">
+          <v-autocomplete label="amount name" variant="outlined" density="compact"></v-autocomplete>
         </v-col>
       </v-row>
     </v-col>
